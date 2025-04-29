a778f9715b203e382bbf9c26a9d8cd1e07e92242
Janani <gjanani2082@gmail.com>
2025-04-29 16:59:38 +0530

    five tables

---
+++ 29-04-2025/5 tables.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class HelloWorld
+{
+    static void Main(String [] args){
+      int num = 5;
+      for(int i=1;i<=10;i++){
+          Console.WriteLine(num +"x" + i +"=" + num*i);
+      }
+    
+}
+
+}
