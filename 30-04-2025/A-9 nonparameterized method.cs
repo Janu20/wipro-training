4d6116bf1fd46eee10c252df45dc1ab3e77c8f8d
Janani <gjanani2082@gmail.com>
2025-04-30 13:38:29 +0530

    non parameter method

---
+++ 30-04-2025/A-9 nonparameterized method.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class HelloWorld
+{
+    public static string method(){
+        string s = "Hello I am Janani from Wipro";
+        return s;
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(method());
+    }
+}
