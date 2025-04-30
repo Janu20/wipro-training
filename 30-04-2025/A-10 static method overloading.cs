0bbbb63f765915f53a6bcb80b31f709463161c83
Janani <gjanani2082@gmail.com>
2025-04-30 14:37:35 +0530

    re commit

--- 30-04-2025/A-10 static method overloading.cs
+++ 30-04-2025/A-10 static method overloading.cs
@@ -10,11 +10,28 @@ public class HelloWorld
     public static double add (int a, double b){
         return a + b;
     }
+    
+    public static int sub(int a, int b){
+        return a-b;
+    }
+     public static double sub(double a, double b){
+        return a-b;
+    }
+     public static double mul(int a, int b){
+        return a*b;
+    }
+     public static double mul(int a, double b){
+        return a*b;
+    }
     public static void Main(string[] args)
     {
        
        Console.WriteLine( add(1,5));
        Console.WriteLine( add(1,5.5));
+       Console.WriteLine(sub(1.5,0.5));
+       Console.WriteLine(sub(2.5,2));
+       Console.WriteLine(mul(5,9));
+       Console.WriteLine(mul(3,2.5));
         
         
         
