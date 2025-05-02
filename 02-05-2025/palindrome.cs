8cf29eb36875acdc4f8ff85528496cdd5ea725ac
Janani <gjanani2082@gmail.com>
2025-05-02 11:10:02 +0530

    palindrome

---
+++ 02-05-2025/palindrome.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Give the word : ");
+        string word = Console.ReadLine();
+        
+        string rev ="";
+        
+        for(int i=word.Length-1;i>=0;i--){
+            rev+=word[i];
+        }
+        if(word.Equals(rev)){
+            Console.WriteLine("It is palindrome");
+        }
+        else{
+            Console.WriteLine("It is not a palindrome");
+        }
+        
+    }
+}
