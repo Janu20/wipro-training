d635c0826ab2d67ab16f24ef8b8513fd0b19de2d
Janu20 <gjanani2082@gmail.com>
2025-05-03 14:31:52 +0530

    CRUD operations

---
+++ 03 may/insert.sql
@@ -0,0 +1,4 @@
+insert into employee values(1,'Janani',22,'Developing',800000);
+insert into employee values(2,'Deepak',22,'Developing',950000);
+insert into employee values(3,'Kumar',25,'Testing',500000);
+insert into employee values(4,'Dev',23,'HR',650000);
