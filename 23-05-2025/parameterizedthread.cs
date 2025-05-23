ce6bd732cf040eacb5e4c15f9c0e9c5269efb788
Janu20 <gjanani2082@gmail.com>
2025-05-23 10:01:40 +0530

    Parameterized thread

---
+++ 23-05-2025/parameterizedthread.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Parameterthread
+    {
+        static void Print(object message)
+        {
+            Console.WriteLine(message);
+            
+        }
+
+        static void Main()
+        {
+            Thread mythread = new Thread  (new ParameterizedThreadStart(Print));
+            mythread.Start("Hello");
+        }
+    }
+}
