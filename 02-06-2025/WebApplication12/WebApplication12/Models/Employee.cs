09670d7eccd0b6f6c97b2e4710e1c89fdd94d68b
Janu20 <gjanani2082@gmail.com>
2025-06-02 17:35:37 +0530

    Web API

---
+++ 02-06-2025/WebApplication12/WebApplication12/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication12.Models
+{
+    public class Employee
+    {
+       public int Id { get; set; }
+       public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public int Salary {  get; set; }
+
+    }
+}
