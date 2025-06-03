e3f6ad02557f67c815df8094a154c6351cd1ab54
Janu20 <gjanani2082@gmail.com>
2025-06-03 17:42:56 +0530

    commit

---
+++ 03-06-2025/WebApplication13/WebApplication13/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication13.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }     
+
+        public string Category { get; set; }
+
+        public string CategoryId { get; set; }
+
+    }
+}
